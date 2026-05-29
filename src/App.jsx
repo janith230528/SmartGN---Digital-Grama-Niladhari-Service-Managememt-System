6914dace057c48b975e1cb1e56969f213aa0e274
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-05-29 20:29:36 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Hero from "./Components/Landing-page/Hero/Hero.jsx";
 import Navbar from "./Components/Landing-page/Navbar/Navbar.jsx";
+import About from "./Components/Landing-page/About/About.jsx";
 import Services from "./Components/Landing-page/Services/Services.jsx";
+import Footer from "./Components/Landing-page/Footer/Footer.jsx";
 
 function App() {
   return (
@@ -9,7 +11,11 @@ function App() {
       <Navbar />
       <Hero />
       <hr/>
+      <About />
+      <hr/>
       <Services />
+      <hr/>
+      <Footer />
       </div>
     </>
   )
